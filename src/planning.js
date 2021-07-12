fda0276b9355345b6c0d56678cf5e9f2ead74b06
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-13 01:24:18 +0530

    routing and layout done

---
+++ src/planning.js
@@ -0,0 +1,23 @@
+//Book my show planning
+
+
+// ->We need a reusable carousel
+// -> hero carousel
+// ->common carousel
+// -> cast and crew small carousel
+
+
+//pages
+//->Home page
+//->Movie page
+//->Categories page
+//->Auth page
+//-Razorpay
+
+
+//plan -> how are we building it
+
+//Layouts
+//Routes
+//Pages
+//Components
