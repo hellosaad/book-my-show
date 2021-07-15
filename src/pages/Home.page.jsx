601d7a69d463909d4063afce0ed45e44bea2a54b
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-15 18:34:24 +0530

    done upto premier carousel

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 //COMPONENTS
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
+import Premier from "../components/Premier/Premier.component";
 const HomePage= () =>{
     return (<>
 
     <div className="container mx-auto px-4">
         <h1 className="text-2xl font-bold text-gray-800 my-3"> The Best Of Entertainment</h1>
         <EntertainmentCardSlider />
+        <Premier />
     </div>
     </>);
 };
