34d82bb602f5bb71d3599986860ec89b7d239327
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-16 17:31:04 +0530

    done upto adding movie page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { Route } from "react-router-dom";
 
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
 // Components
 import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
 
 //import css files
 import "slick-carousel/slick/slick.css";
@@ -13,7 +15,9 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <>
+      
       <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component ={Movie}   />
     </>
   );
 }
