6f15c988e8fc3fa85692871447b99705943b35f9
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-17 18:07:55 +0530

    done upto movie page for all devices

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,9 +1,9 @@
-import React from "react"
+import MovieHero from "../components/MovieHero/MovieHero.component";
 
  const Movie = () => {
     return (
         <>
-          HI  
+          <MovieHero />
         </>
     )
 };
