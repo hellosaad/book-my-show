2b508a5e70d2c20291d5040f9aacf1fceb3783f2
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-22 20:18:53 +0530

    plays page doen

--- src/components/PosterSlider/PosterSlider.component.jsx
+++ src/components/PosterSlider/PosterSlider.component.jsx
@@ -5,14 +5,17 @@ import Poster from "../Poster/Poster.component";
 
 ///config
 import PosterCarouselSettings from "../../config/PosterCarousel.config";
+import Cast from '../Cast/Cast.component';
  const PosterSlider = (props) => {
+  const sliderConfig = props.config ? props.config : PosterCarouselSettings;
+
     return (
-        <>
+      <>
         <div className="flex-col items-start py-2">
         <h3 className={`text-white text-2xl font-bold ${props.isDark ? "text-white" : "text-gray-800"}`}>{props.title}</h3>
         <p className={`text-sm ${props.isDark ? "text-white" : "text-gray-800"}`}>{props.subtitle}</p>
       </div>
-      <Slider {...PosterCarouselSettings}>
+      <Slider {...sliderConfig}>
         {props.images.map((image) => (
           <Poster {...image}  isDark={props.isDark}/>
         ))}
