d23a80744a925fb579990cb8fb200ba10f2e69c6
Saad Mohammed Khaled <saadmohammedkhaled@Saads-MacBook-Air.local>
2021-07-23 23:54:10 +0530

    done upto linking axios for main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { Route } from "react-router-dom";
+import axios from "axios";
 
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
 
-// Components
+// Pages
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
 import Plays from "./pages/Plays.page";
 
-//import css files
+// Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = "391e808c2a1593870640bc32547cea9b";
 function App() {
   return (
     <>
-      
       <DefaultHOC path="/" exact component={HomePage} />
-      <MovieHOC path="/movie/:id" exact component ={Movie}   />
-      <DefaultHOC path="/plays" exact component={Plays} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
+      <DefaultHOC path="/plays" exact component={Plays} /> 
+    
     </>
   );
 }
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, {useState,useEffect}from "react";
+import axios from "axios";
 
 //COMPONENTS
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
@@ -8,7 +8,46 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 //config
 import TempPosters from "../config/TempPosters.config"
 
+
 const HomePage= () =>{
+    const [popularMovies,setPopularMovies] = useState([]);
+    const [topRatedMovies,setTopRatedMovies] = useState([]);
+    const [upcomingMovies,setUpcomingMovies] = useState([]);
+
+
+
+    useEffect(() => {
+        const requestPopularMovies = async() =>{
+            const getPopularMovies = await axios.get("/movie/popular");
+            setPopularMovies(getPopularMovies.data.results);
+        };
+        requestPopularMovies();
+
+    },[]);
+
+    useEffect(() => {
+        const requestTopRatedMovies = async() =>{
+            const getTopRatedMovies = await axios.get("/movie/top_rated");
+            setTopRatedMovies(getTopRatedMovies.data.results);
+        };
+        requestTopRatedMovies();
+
+    },[]);
+
+    useEffect(() => {
+        const requestUpcomingMovies = async() =>{
+            const getUpcomingMovies = await axios.get("/movie/upcoming");
+            setUpcomingMovies(getUpcomingMovies.data.results);
+        };
+        requestUpcomingMovies();
+
+    },[]);
+
+    
+    
+    
+    
+    
     return (<>
     <div className="flex flex-col gap-10">
     <div className="container mx-auto px-4">
@@ -23,12 +62,12 @@ const HomePage= () =>{
            
                 <img 
                 src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" 
-                alt="Premier" 
+                alt="Rupay" 
                 className="w-full h-full"/>
             
         </div>
             <PosterSlider 
-            images={TempPosters} 
+            images={popularMovies} 
             title="Premiers" 
             subtitle="Brand New Releases Every Friday"
             isDark />
@@ -38,14 +77,14 @@ const HomePage= () =>{
     
     <div className="container mx-auto px-4 py-8">
         <PosterSlider 
-        images={TempPosters} 
+        images={topRatedMovies} 
         title="Online Streaming Events" 
         isDark={false}/>
     </div>
 
     <div className="container mx-auto px-4 py-8">
         <PosterSlider 
-        images={TempPosters} 
+        images={upcomingMovies} 
         title="Outdoor Events" 
         isDark={false}/>
     </div>
